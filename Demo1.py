7826d5d1b8ce7df80a8bb07edd3bc07c0a3ddfe1
samir711 <samirdoc1@gmail.com>
2018-05-13 23:31:53 -0400

    First commit

---
+++ Demo1.py
@@ -0,0 +1,7 @@
+age = int(input('Enter your Age : '))
+
+if age >= 18:
+    print('Your are an Adult. ')
+else:
+    print('You are not an Adult. ')
+
---
+++ ListDemo.py
@@ -0,0 +1,10 @@
+# names = ["Mike", "John", "Rob"]
+#
+# print(names[0])
+
+# numbers = [1, 2, 3, 4, 5]
+#
+# print(numbers)
+
+abc = []
+print(abc)
