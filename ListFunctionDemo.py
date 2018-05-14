aa1823cc58dae289e3b1070a9700df3d3f105d31
samir711 <samirdoc1@gmail.com>
2018-05-14 00:00:02 -0400

    First commit

---
+++ ListFunctionDemo.py
@@ -0,0 +1,22 @@
+# List Append
+# fruits = ['Apple', 'Mango', ' Peach', 'Orange', 'Banana']
+#
+# fruits.append("Pineapple")
+# print(fruits)
+
+# List Length
+# fruits = ['Apple', 'Mango', ' Peach', 'Orange', 'Banana']
+# print(len(fruits))
+
+# List Insert
+
+# fruits = ['Apple', 'Mango', ' Peach', 'Orange', 'Banana']
+# fruits.insert(0, 'Pineapple')
+# print(fruits)
+
+# List Index
+
+fruits = ['Apple', 'Mango', 'Peach', 'Orange', 'Banana']
+
+print(fruits.index('Peach'))
+
---
+++ ListOperationDemo.py
@@ -0,0 +1,17 @@
+fruits = ['Apple', 'Mango', 'Peach', 'Orange']
+
+print("Orange" in fruits)
+
+
+# numbers = [1, 1, 1, 1, 1, 1]
+#
+# secondnumbers = [2, 2, 2, 2, 2]
+#
+# print(numbers * 3)
+
+
+# print(numbers + secondnumbers)
+
+# numbers[2] = 5
+#
+# print(numbers)
