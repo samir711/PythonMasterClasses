0e335fa108de25f43cbd357ba88596b511c2fd3e
samir711 <samirdoc1@gmail.com>
2018-05-13 23:03:52 -0400

    First commit

---
+++ Demo.py
@@ -0,0 +1,11 @@
+p = input('Enter value for Principal : ')
+n = input('Enter value for Number of Years : ')
+r = input('Enter value for Rate of Interest  : ')
+
+p = int(p)
+n = int(n)
+r = int(r)
+
+si = (p * n * r) / 100
+
+print(si)
