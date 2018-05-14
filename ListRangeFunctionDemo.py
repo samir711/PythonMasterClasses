bc9c8f5fcd2a3bccee0e4ae09e86f3c8926cace3
samir711 <samirdoc1@gmail.com>
2018-05-14 00:19:35 -0400

    First commit

---
+++ ListRangeFunctionDemo.py
@@ -0,0 +1,13 @@
+# List fixed Range
+# numbers = list(range(10))
+# print(numbers)
+
+# List specific  Range
+
+# numbers = list(range(1, 30))
+# print(numbers)
+
+# List specific  Range with Range
+
+numbers = list(range(1, 100, 3))
+print(numbers)
